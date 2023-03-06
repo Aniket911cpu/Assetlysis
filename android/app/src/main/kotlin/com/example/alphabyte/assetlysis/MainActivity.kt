7b959b2b8b63f255ed3f3ba6f6ead621d9a2eed4
MasterAniket <aniketmaji265@gmail.com>
2023-03-06 18:03:40 +0530

    project Started

---
+++ android/app/src/main/kotlin/com/example/alphabyte/assetlysis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alphabyte.assetlysis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class hme extends StatefulWidget {
+  const hme({Key? key}) : super(key: key);
+
+  @override
+  State<hme> createState() => _hmeState();
+}
+
+class _hmeState extends State<hme> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+      ),
+    );
+  }
+}
